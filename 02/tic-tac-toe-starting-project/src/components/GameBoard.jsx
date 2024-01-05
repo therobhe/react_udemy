d031ce4737664400e31e0e95214212ccf7f60220
Robert Heinemann <r.heinemann@group-apo.com>
2024-01-05 14:58:16 +0100

    L80: Initial logic to update the gameboard using previous state to create the next state

--- 02/tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 02/tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,14 +1,32 @@
+import {useState} from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 export default function GameBoard() {
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard)
+
+    /**
+     * Create a immutable next gameboard from the previous one and update the clicked cell
+     * @param rowIndex
+     * @param colIndex
+     * @param symbol
+     */
+    const handleSelectedSquare = (rowIndex, colIndex, symbol) => {
+        setGameBoard((gameBoard) => {
+            let nextGameBoard = [...gameBoard.map(innerArray => [...innerArray])]
+                nextGameBoard[rowIndex][colIndex] = symbol
+            return nextGameBoard
+        })
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleSelectedSquare(rowIndex, colIndex, 'X')}>{playerSymbol}</button></li>)}
                 </ol>
             </li>
             )}
