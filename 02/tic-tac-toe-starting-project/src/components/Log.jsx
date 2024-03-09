637983ba4219767a306dc7b60c676ea777e710c5
Robert Heinemann <r.heinemann@group-apo.com>
2024-03-09 23:05:16 +0100

    L85: State across components

--- 02/tic-tac-toe-starting-project/src/components/Log.jsx
+++ 02/tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,7 +1,12 @@
-export default function Log({ gameTurns }) {
+export default function Log({gameTurns}) {
     return (
         <>
-            {gameTurns.forEach((turn) => console.log(turn))}
+            <h1>Log</h1>
+            <ol>
+                {gameTurns.map((turn) => {
+                   return <li key={`${turn.square.row}${turn.square.col}`}>{turn.player} selected {turn.square.row}, {turn.square.col}</li>
+                })}
+            </ol>
         </>
     )
 }
