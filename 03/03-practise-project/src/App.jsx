65035ea7e8458c1627b6d5510ec9e86a4523cd2b
Robert Heinemann <r.heinemann@group-apo.com>
2024-03-27 12:12:42 +0100

    Section 5: Practice Project

---
+++ 03/03-practise-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/sections/Header.jsx'
+import UserInput from './components/sections/UserInput.jsx'
+import Result from './components/sections/Result.jsx'
+function App() {
+    /**
+     * Get values from UserInput and send them to results
+     */
+  return (
+      <>
+        <Header />
+        <UserInput />
+        <Result />
+      </>
+  )
+}
+
+export default App
