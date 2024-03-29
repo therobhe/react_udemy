3704ad5731c4470b4137b9ea08501f14dffe6ec7
Robert Heinemann <r.heinemann@group-apo.com>
2024-03-29 18:50:09 +0100

    Section 6: Styling React Components - conditional inline styling.

--- 04/04-styling-components/src/components/Header.jsx
+++ 04/04-styling-components/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import logo from '../assets/logo.png';
 
 export default function Header() {
+    const day = new Date().getDay()
+    const isEven = (day % 2) === 0
+
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <h1 style={{
+          color: isEven ? "blue" : "red"
+      }}>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
