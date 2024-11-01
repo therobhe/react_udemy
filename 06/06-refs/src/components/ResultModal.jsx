18dbc30e4ac2d593273b621e1c990dcf905cd082
r.heinemann <r.heinemann@group-apo.com>
2024-11-01 15:33:31 +0100

    [S8] Improve Code Separation by introducing component apis with useImperativeHandle()

--- 06/06-refs/src/components/ResultModal.jsx
+++ 06/06-refs/src/components/ResultModal.jsx
@@ -1,10 +1,13 @@
 /* forwarding a ref to sub-components does not work like with state by default
-*   therefore, we need to forward it manually
-* */
+ * therefore, we need to forward it manually
+ * */
 import {forwardRef, useRef, useImperativeHandle} from "react";
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
-        // detatches the TimerChallenge from the modal by presenting an component API
+        /*
+         * detatches the TimerChallenge from the modal by presenting an component API
+         * useImperativeHandle() will most likely always be used in combination with forwardRef()
+         */
         const refDialog = useRef();
         useImperativeHandle(ref, () => ({
             open: () => refDialog.current.showModal(),
