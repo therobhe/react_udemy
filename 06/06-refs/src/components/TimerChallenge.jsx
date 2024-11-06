dee2139d262c2c715ce7238488c12a556a333b3a
Robert Heinemann <robert.heinemann@s2015.tu-chemnitz.de>
2024-11-07 00:07:06 +0100

    [S8] Adding logic for winning condition and time measurement

--- 06/06-refs/src/components/TimerChallenge.jsx
+++ 06/06-refs/src/components/TimerChallenge.jsx
@@ -2,29 +2,54 @@ import {useRef, useState} from "react";
 import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge ({ title, targetTime }) {
-    const [timerRunning, setTimerRunning] = useState(false);
-    const [timerDone, setTimerDone] = useState(false);
-
     // here, the ref is used to store the pointer on the timer so it can be referenced in the handleStor function
     const refTimer = useRef();
     const refDialog = useRef();
 
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+    const timerActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+    // check if time is up and reset the timer, show losing modal
+    if (timeRemaining <= 0) {
+        clearInterval(refTimer.current);
+        setTimeRemaining(targetTime * 1000);
+        refDialog.current.open();
+    }
+
+    // new approach in order to track remaining time with setInterval
     const handleStart = () => {
-        refTimer.current = setTimeout(() => {
-            setTimerDone(true);
-            setTimerRunning(false);
-
-            // show the lose modal after time is up
-            refDialog.current.open();
-        }, targetTime * 1000);
-
-        setTimerRunning(true);
+        refTimer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10) // re-executes the function every 10ms
     }
 
     const handleStop = () => {
-        clearTimeout(refTimer.current);
-        setTimerRunning(false);
+        refDialog.current.open();
+        clearInterval(refTimer.current);
     }
+
+    /**
+     * Old way does not prepare for winning condition
+     *
+     *     const handleStart = () => {
+     *         refTimer.current = setTimeout(() => {
+     *             setTimerDone(true);
+     *             setTimerRunning(false);
+     *
+     *             // show the lose modal after time is up
+     *             refDialog.current.open();
+     *         }, targetTime * 1000);
+     *
+     *         setTimerRunning(true);
+     *     }
+     *
+     *     const handleStop = () => {
+     *         clearTimeout(refTimer.current);
+     *         setTimerRunning(false);
+     *     }
+     */
+
 
     return (
         <>
@@ -37,13 +62,11 @@ export default function TimerChallenge ({ title, targetTime }) {
                     {targetTime} second{targetTime > 1 ? '' : 's'}
                 </p>
 
-                {timerDone && <p>You Lost!</p>}
-
                 <p>
-                    <button onClick={timerRunning ? handleStop : handleStart}>{timerRunning ? 'Stop' : 'Start'} Challenge</button>
+                    <button onClick={timerActive ? handleStop : handleStart}>{timerActive ? 'Stop' : 'Start'} Challenge</button>
                 </p>
-                <p className={timerRunning ? 'active' : undefined}>
-                    Timer {timerRunning ? 'running' : 'inactive'}
+                <p className={timerActive ? 'active' : undefined}>
+                    Timer {timerActive ? 'running' : 'inactive'}
                 </p>
             </section>
         </>
