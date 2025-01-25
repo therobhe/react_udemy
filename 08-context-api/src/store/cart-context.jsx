6ff4b7d1ab7b4a3bede02c9d07e0531c05a4aca5
robhe <rheinemann97@gmail.com>
2025-01-25 08:38:41 +0100

    S08 - Prop Drilling Prevention Method 2: Context API.
    - useReducer Hook to simplify complexety of state management logic
    - instead of large extra functions, we dispatch actions so that the state management can be outsourced and called action-based in the reducer function

--- 08-context-api/src/store/cart-context.jsx
+++ 08-context-api/src/store/cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 /* initial value helps to code complete */
@@ -8,76 +8,105 @@ export const CartContext = createContext({
   updateCartQuantity: () => {}
 });
 
-
-/* all the logic for managing the updating state that was previously placed in the app component is now here in our custom Provider */
-export default function CartContextProvider({ children }) {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: []
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
+/*
+  function that is immediately called be the useReducer hook
+  - state is the guaranteed latest state snapshot (like prevState)
+  - action is the action that was dispatched with useReducer
+*/
+function shoppingCartReducer(state, action) {
+  if (action.type === 'ADD_ITEM') {
+      const updatedItems = [...state.items];
 
       const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
+        (cartItem) => cartItem.id === action.payload
       );
       const existingCartItem = updatedItems[existingCartItemIndex];
 
       if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
+          const updatedItem = {
+              ...existingCartItem,
+              quantity: existingCartItem.quantity + 1
+          };
+          updatedItems[existingCartItemIndex] = updatedItem;
       } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1
-        });
+          const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+          updatedItems.push({
+              id: action.payload,
+              name: product.title,
+              price: product.price,
+              quantity: 1
+          });
       }
 
       return {
-        items: updatedItems
+          ...state,
+          items: updatedItems
       };
+
+  }
+  if (action.type === 'UPDATE_ITEM') {
+        const updatedItems = [...state.items];
+        const updatedItemIndex = updatedItems.findIndex(
+          (item) => item.id === action.payload.productId
+        );
+
+        const updatedItem = {
+            ...updatedItems[updatedItemIndex]
+        };
+
+        updatedItem.quantity += action.payload.amount;
+
+        if (updatedItem.quantity <= 0) {
+            updatedItems.splice(updatedItemIndex, 1);
+        } else {
+            updatedItems[updatedItemIndex] = updatedItem;
+        }
+
+        return {
+            ...state,
+            items: updatedItems
+        };
+  }
+
+  return state;
+}
+
+/* all the logic for managing the updating state that was previously placed in the app component is now here in our custom Provider */
+export default function CartContextProvider({ children }) {
+  /*
+  to simplify the multiple state update functions that rely on the previous state, we can use a reducer function
+  - it takes two arguments: an initial state and a dispatch function that is used to trigger actions which then will
+    be handled by the reducer function
+   - the useReducer hook takes the reducer function and the initial state as arguments and returns the current state
+  */
+  const [shoppingCartState, dispatchShoppingCartAction] = useReducer(shoppingCartReducer, {
+      items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    dispatchShoppingCartAction({
+        type: 'ADD_ITEM',
+        payload: id
     });
   }
 
   function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex]
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems
-      };
-    });
+      dispatchShoppingCartAction({
+          type: 'UPDATE_ITEM',
+          payload: {
+              productId,
+              amount
+          }
+      });
   }
 
   /*
    this links the dynamic updating state to the context:
   - items is for the reading the state-items in the cart component through the context
-  - addItemToCart is for the writing in the product component through the contextt
+  - addItemToCart is for the writing in the product component through the context
   */
   const cartCtx = {
-    items: shoppingCart.items,
+    items: shoppingCartState.items,
     addItemToCart: handleAddItemToCart,
     updateCartQuantity: handleUpdateCartItemQuantity
   };
