64380f714b5b2c01828dcc6840b89358c6592c10
robhe <rheinemann97@gmail.com>
2025-02-05 19:16:15 +0100

    S09 useEffect:
    - introduce useEffect to prevent infinite loop

--- 09-useEffect/src/App.jsx
+++ 09-useEffect/src/App.jsx
@@ -1,15 +1,27 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
   const modal = useRef();
-  const selectedPlace = useRef();
+  const selectedPlace = useRef([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  // a side effect is something that is not directly related to the rendering of the (App) component (it is not setting up a click listener, or changing the state of the component)
+  // useEffect is a hook that allows you to perform side effects in function components without triggering a re-render (here it prevents an infinite loop)
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES, position.coords.latitude, position.coords.longitude);
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []); // the code will be evaluated as the last step after the first render
+  // without dependencies, it will only run once after the first render and is then ignored
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +40,9 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const selectedPlaces = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem("selectedPlaces", JSON.stringify(id, ...selectedPlaces));
   }
 
   function handleRemovePlace() {
@@ -57,13 +72,14 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
