b1ce89f5caee27f0373e39a88010d81cc56f0098
robhe <rheinemann97@gmail.com>
2025-02-06 23:39:20 +0100

    S07 updating practise project for showcase:
    - add responsiveness

--- 07-practise-project/src/components/ProjectList.jsx
+++ 07-practise-project/src/components/ProjectList.jsx
@@ -7,11 +7,11 @@
  */
 export default function ProjectList({ projects, handleEdit }) {
   return (
-    <ul className="mt-8">
+    <ul className="mt-6">
       {projects.map((project) => (
         <li key={project.id}>
           <button
-            className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800"
+            className="w-full text-left px-2 py-1 rounded-md my-1 hover:text-stone-200 hover:bg-stone-800"
             onClick={() => handleEdit(project.id)}
           >
             {project.title}
--- 07-practise-project/src/components/ProjectSidebar.jsx
+++ 07-practise-project/src/components/ProjectSidebar.jsx
@@ -18,15 +18,17 @@ export default function ProjectSidebar({ handleEdit, handleCreate }) {
 
   return (
     <aside className="p-6 bg-stone-900 text-stone-50 rounded-r-xl w-1/3">
-      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
-        Your Projects
-      </h2>
-      <button
-        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-        onClick={handleCreate}
-      >
-        + Add Project
-      </button>
+      <div className="border-b border-stone-700 pb-6">
+        <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+          Your Projects
+        </h2>
+        <button
+          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          onClick={handleCreate}
+        >
+          + Add Project
+        </button>
+      </div>
 
       {hasProjects && (
         <ProjectList projects={projects} handleEdit={handleEdit} />
