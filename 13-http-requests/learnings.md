d8d0f1bd7a53ba0da06c87ed9409d5edc6225d59
robhe <rheinemann97@gmail.com>
2025-07-22 12:07:54 +0200

    S15: HTTP Requests:
    - loading state and learnings

--- 13-http-requests/learnings.md
+++ 13-http-requests/learnings.md
@@ -1,5 +1,11 @@
 # Section 15: HTTP Requests
-- Clienttside React code runs in the browser of a user. Direct DB access is not possible due to security reasons, since the code is visible to the user.
+- Client side React code runs in the browser of a user. Direct DB access is not possible due to security reasons, since the code is visible to the user.
 - To access a database, we need to use a server-side code that can securely interact with the database.
 - Communication is happening with HTTP-Requests to endpoints (URLs), that the Server knows, so that he manages what a user can and cannot do
 - a REST-API is a web server that exposes pre-defined routes to which HTTP-Requests can be sent
+
+## Implementation reminder
+- use `fetch` to send HTTP-Requests inside the useEffect hook to prevent infinite loops
+- `fetch` returns a promise, so we need to use `.then()` to handle the response or await the response
+- resolve the promise with `response.json()` to get the data in JSON format
+- implementing `loadingState` and `errorState` is a good practice to handle the state of the request
--- 13-http-requests/src/components/AvailablePlaces.jsx
+++ 13-http-requests/src/components/AvailablePlaces.jsx
@@ -3,20 +3,31 @@ import { useEffect, useState } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
-  // TODO: Fetch available places from the server
-  useEffect(() =>{
-    fetch('http://localhost:3000/places')
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true); // toggle fetching state
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+    fetchPlaces();
+
+/*    fetch('http://localhost:3000/places')
       .then((response) => {return response.json()}) // resoles the promise
       .then((responseData) => {
         setAvailablePlaces(responseData.places) // have a look at the backend code to see what the response looks like
-      })
+      })*/
   }, [])
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data...."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
