1a476886580d3daf33d14842d0207378d5f1471e
robhe <rheinemann97@gmail.com>
2025-07-24 11:58:55 +0200

    S18: Handling form submissions with backend
    - sending the validated form data to the backend-endpoint

--- 16_2-sending-form-data-to-backend/src/components/NewOpinion.jsx
+++ 16_2-sending-form-data-to-backend/src/components/NewOpinion.jsx
@@ -1,23 +1,79 @@
+import { useActionState, useContext } from "react";
+import { OpinionsContext } from "../store/opinions-context.jsx";
+
 export function NewOpinion() {
+  const { addOpinion } = useContext(OpinionsContext);
+
+  const submitNewOpinion = (prevState, formData) => {
+    const name = formData.get("userName");
+    const title = formData.get("title");
+    const body = formData.get("body");
+
+    let error = [];
+
+    if (name.length < 1) {
+      error.push("User name is required.");
+    }
+
+    if (title.length < 1) {
+      error.push("Title is required.");
+    }
+
+    if (body.length < 1) {
+      error.push("Body is required.");
+    }
+
+    if (error.length > 0) {
+      return {
+        error,
+        enteredData: {
+          name,
+          title,
+          body
+        }
+      };
+    }
+
+    console.log("Sending HTTP request to save opinion...");
+    const requestBody = {
+      userName: name,
+      title,
+      body
+    };
+    addOpinion(requestBody);
+
+    return { error: null };
+  };
+
+  const [formData, formAction, pending] = useActionState(submitNewOpinion, { error: null });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input type="text" id="userName" name="userName" defaultValue={formData.enteredData?.name} />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input type="text" id="title" name="title" defaultValue={formData.enteredData?.title} />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea id="body" name="body" rows={5} defaultValue={formData.enteredData?.body}></textarea>
         </p>
+
+        {formData.error && (
+          <ul className="error-list">
+            {formData.error.map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
 
         <p className="actions">
           <button type="submit">Submit</button>
