8c37981b93466ce57496a14ea2a69400f6d22bc0
robhe <rheinemann97@gmail.com>
2025-08-12 16:36:45 +0200

    S20 Advanced Redux:
    - action creator thunk approach

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -3,8 +3,8 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./store";
 import Notification from "./components/UI/Notification";
+import { sendCartToFirebase } from "./store/cart-slice";
 
 // things defined here wont run on each re-render but only on parsing the file (effectively just once)
 let isInitial = true;
@@ -16,41 +16,12 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    async function sendCartToFirebase() {
-      dispatch(uiActions.showNotification({ status: "pending", title: "sending data..", message: "sending data to firebase..." }));
-
-      const response = await fetch("https://udemy-testing-a7fd5-default-rtdb.firebaseio.com/cart.json", {
-        method: "PUT",
-        body: JSON.stringify(cart)
-      });
-
-      if (!response.ok) {
-        throw new Error(response.message);
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Successfully sent!",
-          message: "Data was sent successfully."
-        })
-      );
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartToFirebase().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "An error has occuerd",
-          message: "Something went wrong when saving the data to firebase"
-        })
-      );
-    });
+    dispatch(sendCartToFirebase(cart));
   }, [cart, dispatch]);
 
   return (
--- 19-advanced-redux/src/store/cart-slice.js
+++ 19-advanced-redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./index";
 
 const initialCartState = {
   items: [{ id: "i1", title: "Test Item", quantity: 1, totalPrice: 18, price: 6 }],
@@ -33,3 +34,43 @@ export const cartSlice = createSlice({
     }
   }
 });
+
+// Action Creator Thunk
+// helps keeping components small since we can execute code before we execute our action
+export function sendCartToFirebase(cart) {
+  // normally we would return the action object {state,action} but by returning a function, we can do custom code instead
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({ status: "pending", title: "sending data..", message: "sending data to firebase..." }));
+
+    const sendRequest = async () => {
+      const response = await fetch("https://udemy-testing-a7fd5-default-rtdb.firebaseio.com/cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error(response.message);
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Successfully sent!",
+          message: "Data was sent successfully."
+        })
+      );
+    };
+
+    try {
+      await sendRequest();
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error appeared!",
+          message: "Data was not send."
+        })
+      );
+    }
+  };
+}
