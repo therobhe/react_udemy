b2810f77a5b8e7387d89a8c7fe97f82ac5c21968
robhe <rheinemann97@gmail.com>
2025-09-03 16:28:22 +0200

    Using useNavigation() to grab the current state of a transition / form submission

--- 21-react-router-advanced/frontend/src/components/EventForm.js
+++ 21-react-router-advanced/frontend/src/components/EventForm.js
@@ -1,8 +1,11 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   const navigate = useNavigate();
   function cancelHandler() {
     navigate('..');
@@ -27,10 +30,10 @@ function EventForm({ method, event }) {
         <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : ""} />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button type='submit'>Save</button>
+        <button type='submit' disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : "Save"}</button>
       </div>
     </Form>
   );
