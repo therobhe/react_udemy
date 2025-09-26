a30e847f9b8255a02fdc78be3afd247cbe9f33ed
robhe <rheinemann97@gmail.com>
2025-09-26 14:41:01 +0200

    S22 Authentication:
    -implement logout

--- 22-authentication/frontend/src/App.js
+++ 22-authentication/frontend/src/App.js
@@ -1,68 +1,67 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import ErrorPage from './pages/Error';
-import EventDetailPage, {
-  loader as eventDetailLoader,
-  action as deleteEventAction,
-} from './pages/EventDetail';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
-import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthenticationPage, { action as authAction } from './pages/Authentication';
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
+import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction } from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage, { action as authAction } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'auth', element: <AuthenticationPage />, action: authAction },
+      { path: "auth", element: <AuthenticationPage />, action: authAction },
+      { path: "logout", action: logoutAction },
       {
-        path: 'events',
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           {
             index: true,
             element: <EventsPage />,
-            loader: eventsLoader,
+            loader: eventsLoader
           },
           {
-            path: ':eventId',
-            id: 'event-detail',
+            path: ":eventId",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
                 index: true,
                 element: <EventDetailPage />,
-                action: deleteEventAction,
+                action: deleteEventAction
               },
               {
-                path: 'edit',
+                path: "edit",
                 element: <EditEventPage />,
-                action: manipulateEventAction,
-              },
-            ],
+                action: manipulateEventAction
+              }
+            ]
           },
           {
-            path: 'new',
+            path: "new",
             element: <NewEventPage />,
-            action: manipulateEventAction,
-          },
-        ],
+            action: manipulateEventAction
+          }
+        ]
       },
       {
-        path: 'newsletter',
+        path: "newsletter",
         element: <NewsletterPage />,
-        action: newsletterAction,
-      },
-    ],
-  },
+        action: newsletterAction
+      }
+    ]
+  }
 ]);
 
 function App() {
--- 22-authentication/frontend/src/components/MainNavigation.js
+++ 22-authentication/frontend/src/components/MainNavigation.js
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { Form, NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
   return (
@@ -9,45 +9,29 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
+            <NavLink to="/" className={({ isActive }) => (isActive ? classes.active : undefined)} end>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/events"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+            <NavLink to="/events" className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Events
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/newsletter"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+            <NavLink to="/newsletter" className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Newsletter
             </NavLink>
           </li>
           <li>
-            <NavLink
-              to="/auth?mode=login"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+            <NavLink to="/auth?mode=login" className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Authentication
             </NavLink>
+          </li>
+          <li>
+            <Form action="/logout" method="post">
+              <button>Logout</button>
+            </Form>
           </li>
         </ul>
       </nav>
