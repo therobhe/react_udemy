f86cd07350805dfc00cf6cc0d60459af7d7fb6a3
robhe <rheinemann97@gmail.com>
2025-09-29 10:30:19 +0200

    S22 Authentication:
    - protect routes from access if not logged in

--- 22-authentication/frontend/src/pages/Authentication.js
+++ 22-authentication/frontend/src/pages/Authentication.js
@@ -47,5 +47,9 @@ export async function action({ request }) {
   const token = responseData.token;
   localStorage.setItem("token", token);
 
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
+
   return redirect("/");
 }
