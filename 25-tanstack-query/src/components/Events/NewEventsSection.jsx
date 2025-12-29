6361d83e7dadc7eea6ade65e4dc4e504ceacdd77
robhe <rheinemann97@gmail.com>
2025-12-29 10:47:34 +0100

    S25: Tanstack Query:
    - configuring the useQuery hook

--- 25-tanstack-query/src/components/Events/NewEventsSection.jsx
+++ 25-tanstack-query/src/components/Events/NewEventsSection.jsx
@@ -7,9 +7,13 @@ import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
   // use Tanstack hook in order to send the http request to our backend
+  // every time the component renders, the hook is executed. So for 1st visit, it actually fetches data and stores the request with the key "events"
+  // but on all other renders, it first shows the data from the cache --> in the background executes the fetch again --> updates data if diff registered
   const {data, isPending, isError, error} = useQuery({
     queryKey: ['events'], // define a key that is used for caching
     queryFn: fetchEvents, // define a promise that runs the http request
+    staleTime: 3000, // do not refetch query if the component is called within 3 seconds
+    gcTime: 60000 // lifetime of the cache - 1 minute
   })
 
   let content;
