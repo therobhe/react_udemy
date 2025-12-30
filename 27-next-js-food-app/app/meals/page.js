c3908e91380390a1d50de9a77085d090762f07f3
robhe <rheinemann97@gmail.com>
2025-12-30 14:59:46 +0100

    S27: Next JS Food App
    - setting up the project for data fetching via a db
    
    - installing sqlite via npm
    
    - setting up sqlite via indexdb.js

--- 27-next-js-food-app/app/meals/page.js
+++ 27-next-js-food-app/app/meals/page.js
@@ -1,9 +1,24 @@
+import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+
 export default function MealsPage() {
+	// db connect & data fetch
+	
 	return (
 		<>
-			<h1 style={{ color: 'white', textAlign: 'center' }}>
-				View all Meals
-			</h1>
+			<header className={classes.header}>
+				<h1>Delicious meals, created by <span className={classes.highlight}>by you</span></h1>
+				<p>Choose your favourite recipe and cook it yourself.</p>
+				<p className={classes.cta}>
+					<Link href="/meals/share">
+						Share your favourite recipe
+					</Link>
+				</p>
+			</header>
+			<main className={classes.main}>
+				<MealsGrid meals={[]} />
+			</main>
 		</>
 	)
 }
