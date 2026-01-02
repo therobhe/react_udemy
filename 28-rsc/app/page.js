61c9b211c2412adb9d81a9866cfbdc8070f58b5e
robhe <rheinemann97@gmail.com>
2026-01-02 16:35:58 +0100

    S28: RSC
    - catching errors in use fetching with errorbooundary

--- 28-rsc/app/page.js
+++ 28-rsc/app/page.js
@@ -1,9 +1,10 @@
+import ErrorBoundary from '@/components/ErrorBoundary'
 import UsePromiseDemo from '@/components/UseDemo'
 import fs from 'node:fs/promises'
 import { Suspense } from 'react'
 
 export default function Home() {
-  const fetchUsersPromise = new Promise(resolve => setTimeout(async () => {
+  const fetchUsersPromise = new Promise((resolve) => setTimeout(async () => {
     const data = await fs.readFile('dummy-db.json', 'utf8')
     const users = JSON.parse(data)
     resolve(users);
@@ -11,9 +12,11 @@ export default function Home() {
   
   return (
     <main>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UsePromiseDemo usersPromise={fetchUsersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback="Something went wrong!">
+        <Suspense fallback={<p>Loading...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
