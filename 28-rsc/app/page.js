9cdfcdf433e43ccdd71f03af00b8325b8d1dfd18
robhe <rheinemann97@gmail.com>
2026-01-02 16:32:33 +0100

    S28: RSC
    - use() Hook for fetching delayed promises in child components

--- 28-rsc/app/page.js
+++ 28-rsc/app/page.js
@@ -1,9 +1,19 @@
-import ServerActionsDemo from '@/components/ServerActionDemo'
+import UsePromiseDemo from '@/components/UseDemo'
+import fs from 'node:fs/promises'
+import { Suspense } from 'react'
 
 export default function Home() {
+  const fetchUsersPromise = new Promise(resolve => setTimeout(async () => {
+    const data = await fs.readFile('dummy-db.json', 'utf8')
+    const users = JSON.parse(data)
+    resolve(users);
+  }, 2000))
+  
   return (
     <main>
-      <ServerActionsDemo />
+      <Suspense fallback={<p>Loading...</p>}>
+        <UsePromiseDemo usersPromise={fetchUsersPromise} />
+      </Suspense>
     </main>
   );
 }
---
+++ 28-rsc/components/UseDemo.js
@@ -0,0 +1,24 @@
+"use client"
+
+import { use } from 'react'
+
+export default function UsePromiseDemo({usersPromise}) {
+	
+	const users = use(usersPromise)
+	
+	return (
+		<div className='rsc'>
+			<h2>RSC with Data Fetching</h2>
+			<p>
+				Uses <strong>async / await</strong> for data fetching.
+			</p>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>
+						{user.name} ({user.title})
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
